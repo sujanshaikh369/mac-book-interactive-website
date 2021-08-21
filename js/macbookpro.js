ef3da5180a084151e839bbaf4c50d36cacc1ce69
sujanshaikh369 <sush11112011@gmail.com>
2021-08-21 23:50:41 +0600

    final update for mackbook

--- js/macbookpro.js
+++ js/macbookpro.js
@@ -1,11 +1,11 @@
-
+// universal declaration
 const allTotalPrice = document.getElementById("all-total");
 const extra = document.getElementById("extra-memory");
 const extraStorage = document.getElementById("extra-storage");
 
+// update total calculation
 function updateTotal() {
     const totalPrice = document.getElementById("total");
-    //  allTotalPrice = document.getElementById("all-total");
     const bestPrice = document.getElementById("best-price");
     const deliveryCost = document.getElementById("delivery-cost");
     const deliveryCharge = parseInt(deliveryCost.innerText);
@@ -16,31 +16,34 @@ function updateTotal() {
     totalPrice.innerText = totalExtra + extraS + deliveryCharge;
     allTotalPrice.innerText = totalExtra + extraS + deliveryCharge;
 }
+
+//promo code calculation
 const discountCode = document.getElementById("pomo-code");
 
 document.getElementById("pomo-Code").addEventListener("click", function () {
     const totalPrice = document.getElementById("total");
     console.log(totalPrice.innerText);
-    // const allTotal = document.getElementById("all-total");
-    // console.log(allTotal);
-    let totalAfterPromo = allTotalPrice.innerText;
+    const totalAfterPromo = allTotalPrice.innerText;
     if (discountCode.value.toLowerCase() == "stevekaku") {
-        /* totalPrice.innerText / 20;
-        allTotal.innerText = totalPrice.innerText; */
         allTotalPrice.innerText = totalAfterPromo - (totalAfterPromo * 20) / 100;
     } else {
-        alert('Invalid Promo');
+        alert('Invalid Promo kaku');
     }
 });
 
+//memory slot gb8 price add calculation
 document.getElementById("memory-gb8").addEventListener("click", function () {
     extra.innerText = "0";
     updateTotal();
 });
+
+//memory slot gb16 price add calculation
 document.getElementById("memory-gb16").addEventListener("click", function () {
     extra.innerText = "180";
     updateTotal();
 });
+
+//ssd-storage 256GB price add calculation
 document
     .getElementById("ssd-storage256")
     .addEventListener("click", function () {
@@ -48,6 +51,8 @@ document
         updateTotal();
         console.log(extraStorage);
     });
+
+//ssd-storage 512GB price add calculation
 document
     .getElementById("ssd-storage512")
     .addEventListener("click", function () {
@@ -55,6 +60,8 @@ document
         updateTotal();
         console.log(extraStorage);
     });
+
+//ssd-storage 1TB price add calculation
 document
     .getElementById("ssd-storage1tb")
     .addEventListener("click", function () {
@@ -63,6 +70,7 @@ document
         console.log(extraStorage);
     });
 
+//slow free delivery cost add calculation
 document
     .getElementById("first-delivery")
     .addEventListener("click", function () {
@@ -70,6 +78,8 @@ document
         deliveryCost.innerText = 0;
         updateTotal();
     });
+
+//fast delivery cost add calculation
 document
     .getElementById("second-delivery")
     .addEventListener("click", function () {
